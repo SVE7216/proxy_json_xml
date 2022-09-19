896cc1e9370ad9c26c55d3d2ae65319759f32e00
SVE7216 <108977738+SVE7216@users.noreply.github.com>
2022-09-19 11:48:59 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -16,20 +16,13 @@ def root(user_post=Body(..., embed=True)):
         'Content-Type': 'application/soap+xml;charset=utf-8'
     }
 
-    body =f'''<?xml version="1.0" encoding="utf-8"?>
-            <soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-             <soap12:Body>
-            <NumberToWords xmlns="http://www.dataaccess.com/webservicesserver/">
-            <ubiNum>{data}</ubiNum>
-             </NumberToWords>
-            </soap12:Body>
-            </soap12:Envelope>'''
+    body = Converter(data).json_to_xml()
 
     url = 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso'
     try:
         response = requests.post(url=url, headers=headers, data=body).text
-        json_obg = Converter(response)
-        json_format = json_obg.xml_to_json()
+        json_format = Converter(response).xml_to_json()
         return json_format
     except Exception as error:
         print(error)
+
