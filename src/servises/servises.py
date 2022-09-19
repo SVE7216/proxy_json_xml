2a44538b1349a0b0e6c0f161745f6f74ad8d213e
SVE7216 <108977738+SVE7216@users.noreply.github.com>
2022-09-19 11:49:36 +0700

    Update servises.py

--- src/servises/servises.py
+++ src/servises/servises.py
@@ -11,3 +11,13 @@ class Converter:
         json_format = json.dumps(xmltodict.parse(self.data))
         return json_format
 
+    def json_to_xml(self):
+        body = f'''<?xml version="1.0" encoding="utf-8"?>
+                    <soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+                     <soap12:Body>
+                    <NumberToWords xmlns="http://www.dataaccess.com/webservicesserver/">
+                    <ubiNum>{self.data}</ubiNum>
+                     </NumberToWords>
+                    </soap12:Body>
+                    </soap12:Envelope>'''
+        return body
